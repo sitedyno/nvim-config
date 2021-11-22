726c4b724d0baf3ad2e9911916104004a867867f
Heath Nail <sitedyno@gmail.com>
2021-11-21 20:22:25 -0500

    Disable treesitter html highlight for now
    * pending https://github.com/nvim-treesitter/nvim-treesitter/issues/1788

--- lua/sd/treesitter.lua
+++ lua/sd/treesitter.lua
@@ -23,6 +23,8 @@ tsconf.setup {
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
+        -- disable html pending https://github.com/nvim-treesitter/nvim-treesitter/issues/1788
+        disable = { 'html' },
     },
     indent = {
         enable = true,
