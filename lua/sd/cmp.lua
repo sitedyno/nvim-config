46d72361aa25887162a80ab8c6bf5f7ca1180859
Heath Nail <sitedyno@gmail.com>
2021-11-24 21:30:18 -0500

    Update opts to option for nvim-cmp

--- lua/sd/cmp.lua
+++ lua/sd/cmp.lua
@@ -61,7 +61,7 @@ cmp.setup {
         { name = 'vsnip' },
         {
             name = 'buffer',
-            opts = {
+            option = {
                 get_bufnrs = function()
                     return vim.api.nvim_list_bufs()
                 end,
