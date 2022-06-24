c2261f9d4699c175877bcf57a6746449c0ff983b
Heath Nail <sitedyno@gmail.com>
2022-06-24 12:50:09 -0400

    Improve completions for git commit messages

--- ftplugin/gitcommit.lua
+++ ftplugin/gitcommit.lua
@@ -1,3 +1,16 @@
-require('cmp').setup.buffer {
-    sources = require('cmp').config.sources { { name = 'conventionalcommits' } },
+local cmp = require 'cmp'
+cmp.setup.buffer {
+    sources = cmp.config.sources({
+        { name = 'conventionalcommits' },
+        { name = 'git' },
+    }, {
+        {
+            name = 'buffer',
+            option = {
+                get_bufnrs = function()
+                    return vim.api.nvim_list_bufs()
+                end,
+            },
+        },
+    }),
 }
