f8d44f85474c3b76b5a32fb43691453eb8ede1ba
Heath Nail <sitedyno@gmail.com>
2022-10-19 12:49:43 -0400

    C-d for "down" feels more intuitive

--- lua/sd/cmp.lua
+++ lua/sd/cmp.lua
@@ -32,8 +32,8 @@ cmp.setup {
         },
     },
     mapping = cmp.mapping.preset.insert {
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
         ['<C-e>'] = cmp.mapping.close(),
         ['<C-y>'] = cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Insert,
