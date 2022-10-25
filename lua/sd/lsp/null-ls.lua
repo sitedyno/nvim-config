7174cc55102a0e519e917dd1725f435a140de3b4
Heath Nail <sitedyno@gmail.com>
2022-10-25 17:04:04 -0400

    Maximizing disk space
    Check if phpcs & psalm are installed in the current project, if not do
    not load.

--- lua/sd/lsp/null-ls.lua
+++ lua/sd/lsp/null-ls.lua
@@ -1,15 +1,37 @@
 local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
-local null_ls = require'null-ls'
+local null_ls = require 'null-ls'
 
 null_ls.setup {
     sources = {
+        -- code actions
+        null_ls.builtins.code_actions.shellcheck,
+        -- diagnostics
+        null_ls.builtins.diagnostics.ansiblelint,
+        null_ls.builtins.diagnostics.checkmake,
+        null_ls.builtins.diagnostics.editorconfig_checker,
+        null_ls.builtins.diagnostics.gitlint,
+        null_ls.builtins.diagnostics.hadolint,
+        -- I think the LSP handles this better
+        -- null_ls.builtins.diagnostics.php,
+        null_ls.builtins.diagnostics.phpcs.with {
+            condition = function(utils)
+                return utils.root_has_file { 'vendor/bin/phpcs' }
+            end,
+        },
+        null_ls.builtins.diagnostics.psalm.with {
+            condition = function(utils)
+                return utils.root_has_file { 'vendor/bin/psalm' }
+            end,
+        },
+        null_ls.builtins.diagnostics.shellcheck,
+        null_ls.builtins.diagnostics.sqlfluff.with {
+            extra_args = { '--dialect', 'sqlite' }, -- change to your dialect
+        },
+        null_ls.builtins.diagnostics.twigcs,
+        null_ls.builtins.diagnostics.zsh,
         -- formatters
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.phpcbf,
-        -- diagnostics
-        null_ls.builtins.diagnostics.phpcs,
-        null_ls.builtins.diagnostics.psalm,
-        null_ls.builtins.diagnostics.php,
     },
     on_attach = function(client, bufnr)
         if client.supports_method 'textDocument/formatting' then
