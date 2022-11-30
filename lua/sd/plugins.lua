c413bd8c8142a5a56d7cac535c8fa59a8f324dcf
Heath Nail <sitedyno@gmail.com>
2022-11-30 10:42:33 -0500

    Update packer.nvim bootstrap to match the docs

--- lua/sd/plugins.lua
+++ lua/sd/plugins.lua
@@ -5,19 +5,18 @@ vim.cmd [[
   augroup end
 ]]
 
-local packer_bootstrap = nil
-local fn = vim.fn
-local install_path = fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path, false, false)) > 0 then
-    packer_bootstrap = fn.system {
-        'git',
-        'clone',
-        '--depth',
-        '1',
-        'https://github.com/wbthomason/packer.nvim',
-        install_path,
-    }
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
 end
+
+local packer_bootstrap = ensure_packer()
 
 return require('packer').startup {
     function(use)
