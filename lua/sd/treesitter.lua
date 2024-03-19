e449e910b62b1922989c353be1de169dc41ca1a5
Heath Nail <sitedyno@gmail.com>
2024-03-19 16:32:47 -0400

    Get the playground working again

--- lua/sd/treesitter.lua
+++ lua/sd/treesitter.lua
@@ -19,6 +19,7 @@ tsconf.setup {
         'markdown_inline',
         'php',
         'phpdoc',
+        'query',
         'rasi',
         'regex',
         'scss',
@@ -42,10 +43,25 @@ tsconf.setup {
             'javascript',
             'json',
             'lua',
+            'ansible',
         },
     },
     incremental_selection = {
         enable = true,
+    },
+    playground = {
+        keybindings = {
+            toggle_query_editor = 'o',
+            toggle_hl_groups = 'i',
+            toggle_injected_languages = 't',
+            toggle_anonymous_nodes = 'a',
+            toggle_language_display = 'I',
+            focus_language = 'f',
+            unfocus_language = 'F',
+            update = 'R',
+            goto_node = '<cr>',
+            show_help = '?',
+        },
     },
     yati = {
         enable = true,
