307cc10c9abe6f4cdbcd2764069db4085bb54ecc
Heath Nail <sitedyno@gmail.com>
2024-03-24 22:21:54 -0400

    Move nvim-cmp setup to plugins/completion.lua

---
+++ lua/sd/plugins/completion.lua
@@ -0,0 +1,40 @@
+return {
+    {
+        'hrsh7th/nvim-cmp',
+        config = function()
+            require 'sd.cmp'
+        end,
+        dependencies = {
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-cmdline',
+            'tamago324/cmp-zsh',
+            { 'petertriho/cmp-git', dependencies = 'nvim-lua/plenary.nvim' },
+            'davidsierradz/cmp-conventionalcommits',
+            -- Adds icons for nvim-cmp
+            'onsails/lspkind-nvim',
+            'saadparwaiz1/cmp_luasnip',
+        },
+    },
+
+    {
+        'L3MON4D3/LuaSnip',
+        config = function()
+            require 'sd.luasnip'
+        end,
+        dependencies = { 'saadparwaiz1/cmp_luasnip' },
+    },
+
+    {
+        'sitedyno/nvim-html-css',
+        branch = 'specify-html-for-treesitter',
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter',
+            'nvim-lua/plenary.nvim',
+        },
+        config = function()
+            require('html-css'):setup {}
+        end,
+    },
+}
--- lua/sd/plugins/init.lua
+++ lua/sd/plugins/init.lua
@@ -54,33 +54,6 @@ return {
     'milisims/nvim-luaref',
 
     {
-        'hrsh7th/nvim-cmp',
-        config = function()
-            require 'sd.cmp'
-        end,
-        dependencies = {
-            'hrsh7th/cmp-nvim-lsp',
-            'hrsh7th/cmp-buffer',
-            'hrsh7th/cmp-path',
-            'hrsh7th/cmp-cmdline',
-            'tamago324/cmp-zsh',
-            { 'petertriho/cmp-git', dependencies = 'nvim-lua/plenary.nvim' },
-            'davidsierradz/cmp-conventionalcommits',
-            -- Adds icons for nvim-cmp
-            'onsails/lspkind-nvim',
-            'saadparwaiz1/cmp_luasnip',
-        },
-    },
-
-    {
-        'L3MON4D3/LuaSnip',
-        config = function()
-            require 'sd.luasnip'
-        end,
-        dependencies = { 'saadparwaiz1/cmp_luasnip' },
-    },
-
-    {
         'nvim-telescope/telescope.nvim',
         config = function()
             require 'sd.telescope'
