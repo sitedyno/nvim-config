7713b02aa31838c7708a3ac83afccc0383526676
Heath Nail <sitedyno@gmail.com>
2025-09-20 14:23:52 -0400

    Update gitsigns mappings for visual selections
    I also tried visual for preview, but you just get the preview for the
    whole hunk

--- lua/sd/gitsigns.lua
+++ lua/sd/gitsigns.lua
@@ -44,8 +44,14 @@ gsigns.setup {
         })
 
         -- Actions
-        map({ 'n', 'v' }, '<leader>hs', ':Gitsigns stage_hunk<CR>', { desc = 'Stage Hunk' })
-        map({ 'n', 'v' }, '<leader>hr', ':Gitsigns reset_hunk<CR>', { desc = 'Reset Hunk' })
+        map('n', '<leader>hs', ':Gitsigns stage_hunk<CR>', { desc = 'Stage Hunk' })
+        map('n', '<leader>hr', ':Gitsigns reset_hunk<CR>', { desc = 'Reset Hunk' })
+        map('v', '<leader>hs', function()
+            gs.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
+        end, { desc = 'Stage Hunk' })
+        map('v', '<leader>hr', function()
+            gs.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+        end, { desc = 'Reset Hunk' })
         map('n', '<leader>hS', gs.stage_buffer, { desc = 'Stage Buffer' })
         map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'Undo Staged Hunk' })
         map('n', '<leader>hR', gs.reset_buffer, { desc = 'Reset Buffer' })
