01e917c22967f24385c949a7fd330c06e08451aa
Heath Nail <sitedyno@gmail.com>
2025-11-24 19:58:13 -0500

    Set key maps for leap

--- lua/sd/plugins/navigation.lua
+++ lua/sd/plugins/navigation.lua
@@ -3,7 +3,8 @@ return {
         enabled = true,
         'ggandor/leap.nvim',
         config = function()
-            require('leap').set_default_mappings()
+            vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap)')
+            vim.keymap.set('n',             'S', '<Plug>(leap-from-window)')
         end,
         dependencies = {
             'tpope/vim-repeat',
