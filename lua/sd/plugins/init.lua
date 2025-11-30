56be2c69db97d8342783635687d05e101f84f7b6
Heath Nail <sitedyno@gmail.com>
2025-11-30 13:20:10 -0500

    Add some kitty plugins

--- lua/sd/plugins/init.lua
+++ lua/sd/plugins/init.lua
@@ -286,4 +286,21 @@ return {
             { '<leader>ld', '<cmd>HawtkeysDupes<cr>', desc = 'List Duplicate Hawtkeys' },
         },
     },
+    {
+        "fladson/vim-kitty",
+        ft = "kitty",
+        -- tag = "*"  -- You can select a tagged version
+    },
+    {
+        'mikesmithgh/kitty-scrollback.nvim',
+        enabled = true,
+        lazy = true,
+        cmd = { 'KittyScrollbackGenerateKittens', 'KittyScrollbackCheckHealth', 'KittyScrollbackGenerateCommandLineEditing' },
+        event = { 'User KittyScrollbackLaunch' },
+        -- version = '*', -- latest stable version, may have breaking changes if major version changed
+        -- version = '^6.0.0', -- pin major version, include fixes and features that do not have breaking changes
+        config = function()
+            require('kitty-scrollback').setup()
+        end,
+    },
 }
