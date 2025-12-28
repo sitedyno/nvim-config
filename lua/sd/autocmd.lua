ba98a8e2939a39ab251c25ed597ef33816679d70
Heath Nail <sitedyno@gmail.com>
2025-12-28 13:42:02 -0500

    Help should be front and center

--- lua/sd/autocmd.lua
+++ lua/sd/autocmd.lua
@@ -1,17 +1,6 @@
 -- From https://github.com/numToStr/dotfiles/blob/49a36214d91221108def81715d1792b90f29cf80/neovim/.config/nvim/lua/numToStr/autocmd.lua
 local ag = vim.api.nvim_create_augroup('sd', { clear = true })
 
--- Open help vertically
-vim.api.nvim_create_autocmd('BufWinEnter', {
-    group = ag,
-    pattern = '*.txt',
-    callback = function()
-        if vim.bo.buftype == 'help' then
-            vim.api.nvim_command 'wincmd L'
-        end
-    end,
-})
-
 -- Highlight the region on yank
 vim.api.nvim_create_autocmd('TextYankPost', {
     group = ag,
--- lua/sd/plugins/init.lua
+++ lua/sd/plugins/init.lua
@@ -314,4 +314,32 @@ return {
             require('kitty-scrollback').setup()
         end,
     },
+    {
+        'Tyler-Barham/floating-help.nvim',
+        config = function ()
+            require('floating-help').setup({
+                width = 80,
+                height = 0.9,
+                position = 'C',
+            })
+            -- Only replace cmds, not search; only replace the first instance
+            local function cmd_abbrev(abbrev, expansion)
+                local cmd = 'cabbr ' .. abbrev .. ' <c-r>=(getcmdpos() == 1 && getcmdtype() == ":" ? "' .. expansion .. '" : "' .. abbrev .. '")<CR>'
+                vim.cmd(cmd)
+            end
+            -- Redirect `:h` to `:FloatingHelp`
+            cmd_abbrev('h',         'FloatingHelp')
+            cmd_abbrev('help',      'FloatingHelp')
+            cmd_abbrev('helpc',     'FloatingHelpClose')
+            cmd_abbrev('helpclose', 'FloatingHelpClose')
+        end,
+        keys = {
+            { '<F1>', '<cmd>FloatingHelpToggle<cr>' },
+        },
+        lazy = false,
+    },
+    {
+        "OXY2DEV/helpview.nvim",
+        lazy = false
+    },
 }
