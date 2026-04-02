5dbdc66b13535a4ba888b99debf09c150a0c4af9
Heath Nail <sitedyno@gmail.com>
2026-04-02 16:19:22 -0400

    Add autocmd for PackChanged & nvim-treesitter

--- init.lua
+++ init.lua
@@ -6,6 +6,14 @@ vim.o.cursorline = true
 vim.o.scrolloff = 10
 vim.o.list = true
 
+vim.api.nvim_create_autocmd('PackChanged', { callback = function(ev)
+	local name, kind = ev.data.spec.name, ev.data.kind
+	if name == 'nvim-treesitter' and kind == 'update' then
+		if not ev.data.active then vim.cmd.packadd('nvim-treesitter') end
+		vim.cmd('TSUpdate')
+	end
+end })
+
 vim.pack.add({
   'https://github.com/nvim-mini/mini.nvim',
   'https://github.com/neovim/nvim-lspconfig',
