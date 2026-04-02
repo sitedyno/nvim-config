dee15b1a05e3fddef4ad86b3c7783093a46dcf23
Heath Nail <sitedyno@gmail.com>
2026-04-02 16:28:49 -0400

    Formatting

--- init.lua
+++ init.lua
@@ -15,16 +15,21 @@ vim.api.nvim_create_autocmd('PackChanged', { callback = function(ev)
 end })
 
 vim.pack.add({
-  'https://github.com/nvim-mini/mini.nvim',
-  'https://github.com/neovim/nvim-lspconfig',
-  'https://github.com/nvim-treesitter/nvim-treesitter',
-  'https://github.com/mason-org/mason.nvim',
+	'https://github.com/nvim-mini/mini.nvim',
+	'https://github.com/neovim/nvim-lspconfig',
+	'https://github.com/nvim-treesitter/nvim-treesitter',
+	'https://github.com/mason-org/mason.nvim',
 	'https://github.com/folke/which-key.nvim',
 	'https://github.com/folke/lazydev.nvim',
 })
-vim.api.nvim_create_autocmd('CmdlineEnter', { once = true, callback = function ()
-    require('mason').setup()
-end})
+
+vim.api.nvim_create_autocmd('CmdlineEnter', {
+	once = true,
+	callback = function ()
+		require('mason').setup()
+	end
+})
+
 vim.api.nvim_create_autocmd('BufEnter', {
 	once = true,
 	pattern = {'*.lua'},
