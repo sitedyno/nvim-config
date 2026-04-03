a57a4ba68682dee857e0d4807d1fdc21f8f15e0e
Heath Nail <sitedyno@gmail.com>
2026-04-03 14:59:51 -0400

    Use vim.opt

--- init.lua
+++ init.lua
@@ -1,12 +1,12 @@
 require('vim._core.ui2').enable({})
 
-vim.o.ignorecase = true
-vim.o.smartcase = true
-vim.o.cursorline = true
-vim.o.scrolloff = 10
-vim.o.list = true
-
-vim.api.nvim_create_autocmd('PackChanged', { callback = function(ev)
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+vim.opt.list = true
 	local name, kind = ev.data.spec.name, ev.data.kind
 	if name == 'nvim-treesitter' and kind == 'update' then
 		if not ev.data.active then vim.cmd.packadd('nvim-treesitter') end
