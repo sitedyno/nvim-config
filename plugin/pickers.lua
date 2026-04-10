6203c13374b080d3a6d0b7745e7fb6107a99d7fc
Heath Nail <sitedyno@gmail.com>
2026-04-09 20:55:59 -0400

    Trying out fzf-lua for pickers
    Good so far

---
+++ plugin/pickers.lua
@@ -0,0 +1,13 @@
+vim.pack.add({
+	'https://github.com/ibhagwan/fzf-lua',
+	'https://github.com/nvim-tree/nvim-web-devicons',
+})
+
+require('nvim-web-devicons').setup()
+---@diagnostic disable-line: assign-type-mismatch
+require('fzf-lua').setup({'ivy', 'hide'})
+
+vim.keymap.set({'n', 'v'}, '<localleader>f', '<cmd>FzfLua global<cr>', { desc = 'Find Files'})
+vim.keymap.set({'n', 'v'}, '<localleader>b', '<cmd>FzfLua buffers<cr>', { desc = 'Buffers'})
+vim.keymap.set({'n', 'v'}, '<localleader>g', '<cmd>FzfLua live_grep<cr>', { desc = 'Grep'})
+vim.keymap.set({'n', 'v'}, '<leader>gs', '<cmd>FzfLua git_status<cr>', { desc = 'Git Status'})
