17e2e5fe8ae7b215f0c9c0792660801c33d68f6c
Heath Nail <sitedyno@gmail.com>
2026-04-09 20:51:02 -0400

    Keymaps via which-key.nvim & hawtkeys.nvim

---
+++ plugin/keymaps.lua
@@ -0,0 +1,15 @@
+vim.pack.add({
+	'https://github.com/folke/which-key.nvim',
+	'https://github.com/nvim-lua/plenary.nvim', -- hawtkeys
+	'https://github.com/sitedyno/hawtkeys.nvim',
+})
+
+require('hawtkeys').setup({
+	leader = ',',
+	powerFingers = { 2, 3, 4, 7, 8, 9 },
+	keyboardLayout = 'qwertyortho'
+})
+
+vim.keymap.set({'n', 'v'}, '<leader>Hs', '<cmd>Hawtkeys<cr>', { desc = 'Hawtkeys Search'})
+vim.keymap.set({'n', 'v'}, '<leader>Hd', '<cmd>HawtkeysDupes<cr>', { desc = 'Hawtkeys find Duplicates'})
+vim.keymap.set({'n', 'v'}, '<leader>Ha', '<cmd>HawtkeysAll<cr>', { desc = 'Hawtkeys find Duplicates'})
