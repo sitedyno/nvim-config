25e6afb91db08d8f924befd7d8ff9fba76515402
Heath Nail <sitedyno@gmail.com>
2026-04-10 13:01:50 -0400

    Add format project command
    Only stylua atm. We'll see how poorly this scales...

--- plugin/tasks.lua
+++ plugin/tasks.lua
@@ -1,3 +1,22 @@
 vim.pack.add({
     'https://github.com/stevearc/overseer.nvim',
 })
+local function format_project()
+    local formatters = {
+        stylua = {
+            cmd = 'stylua -g **/*.lua',
+            config = 'stylua.toml',
+        },
+    }
+    for formatter, config in pairs(formatters) do
+        vim.print(formatter)
+        vim.print(config)
+        local found = vim.fn.filereadable(config.config)
+        if found then
+            local task = require('overseer.task').new { cmd = config.cmd }
+            task:start()
+        end
+    end
+end
+
+vim.keymap.set({ 'n', 'v' }, '<leader>fp', format_project, { desc = 'Format Project' })
