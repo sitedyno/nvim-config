626a2649f6812007d77b4fa3e5de8612fde5288d
Heath Nail <sitedyno@gmail.com>
2026-04-17 17:27:36 -0400

    Sonokai

--- init.lua
+++ init.lua
@@ -91,6 +91,13 @@ vim.diagnostic.config {
     underline = false,
 }
 
-vim.pack.add({'https://github.com/scottmckendry/cyberdream.nvim'})
-require('cyberdream').setup({transparent = true})
-vim.cmd.colorscheme('cyberdream')
+vim.pack.add({'https://github.com/sainnhe/sonokai'})
+vim.g.sonokai_style = 'andromeda'
+vim.g.sonokai_enable_italic = 1
+vim.g.sonokai_cursor = 'auto'
+vim.g.sonokai_transparent_background = 1
+vim.g.sonokai_float_style = 'bright'
+vim.g.sonokai_diagnostic_text_highlight = 1
+vim.g.sonokai_diagnostic_line_highlight = 1
+vim.g.sonokai_diagnostic_virtual_text = 'highlighted'
+vim.cmd.colorscheme('sonokai')
