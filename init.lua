76e9d13fd5db8b288f2d8640dd841cfd73439557
Heath Nail <sitedyno@gmail.com>
2026-04-16 23:58:35 -0400

    Cyberdream colorscheme for a bit

--- init.lua
+++ init.lua
@@ -90,3 +90,7 @@ vim.diagnostic.config {
     },
     underline = false,
 }
+
+vim.pack.add({'https://github.com/scottmckendry/cyberdream.nvim'})
+require('cyberdream').setup({transparent = true})
+vim.cmd.colorscheme('cyberdream')
