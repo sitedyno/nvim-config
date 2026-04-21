03e7fa0b0a3a7c8bc36ab45177bf534fe4880856
Heath Nail <sitedyno@gmail.com>
2026-04-21 15:56:18 -0400

    Actually setup leap
    - f/t like flit.nvim
    - s + 2 chars then first match or use the labels
    - S + 2 chars then first match or use the labels in another window
    - <cr> & <bs> can be used to navigate between matches and repeat the search

--- plugin/navigation.lua
+++ plugin/navigation.lua
@@ -4,6 +4,38 @@ vim.pack.add {
     'https://github.com/markstory/vim-zoomwin',
 }
 
+
+local function flit(key_specific_args)
+    require('leap').leap(
+        vim.tbl_deep_extend('keep', key_specific_args, {
+            inputlen = 1,
+            inclusive = true,
+            opts = {
+                -- Force autojump.
+                labels = '',
+                -- Match the modes where you don't need labels (`:h mode()`).
+                safe_labels = vim.fn.mode(1):match('o') and '' or nil,
+            },
+        })
+    )
+end
+
+-- A helper function making it easier to set "clever-f" behavior
+-- (using f/F or t/T instead of ;/, - see the plugin clever-f.vim).
+local clever = require('leap.user').with_traversal_keys
+local clever_f, clever_t = clever('f', 'F'), clever('t', 'T')
+
+-- For relative directions, set the `backward` flags according to:
+-- local prev_backward = require('leap').state['repeat'].backward
+vim.keymap.set({ 'n', 'x', 'o' }, 'f', function() flit { opts = clever_f } end)
+vim.keymap.set({ 'n', 'x', 'o' }, 'F', function() flit { backward = true, opts = clever_f } end)
+vim.keymap.set({ 'n', 'x', 'o' }, 't', function() flit { offset = -1, opts = clever_t } end)
+vim.keymap.set({ 'n', 'x', 'o' }, 'T', function() flit { backward = true, offset = 1, opts = clever_t } end)
+vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)', { desc = 'Type 2 characters of your search'})
+vim.keymap.set({ 'n' }, 'S', '<Plug>(leap-from-window)', { desc = 'Type 2 characters of your search'})
+vim.keymap.set({ 'n', 'x', 'o' }, '<cr>', function() require('leap').leap { ['repeat'] = true, opts = clever('<cr>', '<bs>'), } end)
+vim.keymap.set({ 'n', 'x', 'o' }, '<bs>', function() require('leap').leap { ['repeat'] = true, opts = clever('<bs>', '<cr>'), backward = true, } end)
+
 -- buffers
 vim.keymap.set({ 'n', 'v' }, '<localleader>p', '<cmd>bprevious<cr>', { desc = 'Previous Buffer' })
 vim.keymap.set({ 'n', 'v' }, '<localleader>n', '<cmd>bnext<cr>', { desc = 'Next Buffer' })
