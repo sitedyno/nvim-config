320ba0eb36427210cc1f21664ef1a1341724d41a
Heath Nail <sitedyno@gmail.com>
2026-04-29 09:24:13 -0400

    Add an `All files` picker

--- plugin/pickers.lua
+++ plugin/pickers.lua
@@ -7,7 +7,14 @@ require('nvim-web-devicons').setup()
 ---@diagnostic disable-next-line: assign-type-mismatch
 require('fzf-lua').setup { 'ivy', 'hide' }
 
-vim.keymap.set({ 'n', 'v' }, '<localleader>f', '<cmd>FzfLua global<cr>', { desc = 'Find Files' })
+local function all_files()
+    require('fzf-lua').files({
+        fd_opts = [[--color=never --type f --type l --hidden --no-ignore]],
+    })
+end
+
+vim.keymap.set({ 'n', 'v' }, '<localleader>a', all_files, { desc = 'All files' })
 vim.keymap.set({ 'n', 'v' }, '<localleader>b', '<cmd>FzfLua buffers<cr>', { desc = 'Buffers' })
+vim.keymap.set({ 'n', 'v' }, '<localleader>f', '<cmd>FzfLua files<cr>', { desc = 'Find Files' })
 vim.keymap.set({ 'n', 'v' }, '<localleader>g', '<cmd>FzfLua live_grep<cr>', { desc = 'Grep' })
 vim.keymap.set({ 'n', 'v' }, '<leader>gs', '<cmd>FzfLua git_status<cr>', { desc = 'Git Status' })
